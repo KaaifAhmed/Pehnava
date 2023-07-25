ddfb3a31d72b6caaa3c328dc1d75a71b41f91454
OnlineSaga <120706508+OnlineSaga@users.noreply.github.com>
2023-07-26 00:28:36 +0500

    Done

---
+++ js/cart.js
@@ -0,0 +1,45 @@
+const cart = {};
+var cartitem;
+var delievery_price = 50
+class Desc {
+    constructor(itemname, qty, individual_price, net_price, color, img, dp) {
+    this.name = itemname
+    this.qty = qty;
+    this.ind_price = individual_price;
+    this.net_price = net_price;
+    this.img = img
+    this.color = color;
+    this.delievery = dp
+    }
+}
+
+function addToCart (itemname,...kwargs) {
+    const item = new Desc(itemname, ...kwargs)
+    cartitem = null;
+    localStorage.clear()
+    // location.reload()
+    localStorage.setItem(itemname, JSON.stringify(item));
+    cartitem = item;
+}
+
+
+
+
+// Rites function starting -----------------------------
+
+
+function proceed () {
+
+
+    itemname = document.getElementById('pr-name').innerText;
+    qty = document.getElementById('prod-qty').innerText;
+    ind_price = document.getElementById('pr-price').innerText;
+    net_price = parseInt(qty)*parseInt(ind_price)
+    color = null;
+    img = document.getElementById('main-img').src;
+
+
+    addToCart(itemname, qty, ind_price, net_price, color, img, delievery_price);
+    
+    document.location.href = '../checkout'
+}
